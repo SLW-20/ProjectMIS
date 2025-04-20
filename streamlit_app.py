38a3b300bc959eb62ef4d91147baa2e53b8b3574
ImHani-1 <443802968@kku.edu.sa>
2025-04-20 21:03:20 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -345,8 +345,8 @@ if not df.empty:
             #model = DecisionTreeRegressor(random_state=50)
             #model = XGBRegressor(n_estimators=100, learning_rate=0.1, random_state=42)
             #model = GradientBoostingRegressor(n_estimators=8000, learning_rate=0.1, random_state=42)
-            model = Lasso(alpha=0.1, random_state=200)
-           # model = Ridge(alpha=1.0, random_state=42)
+            #model = Lasso(alpha=0.1, random_state=200)
+            model = Ridge(alpha=1.0, random_state=42)
 
             
            
