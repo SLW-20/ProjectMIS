c6907435d7123da82921e3b74142c2d805878b3d
M7MD <slw6341@gmail.com>
2025-04-20 16:36:26 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -333,7 +333,7 @@ if not df.empty:
             X = pd.get_dummies(data[['neighborhood_name', 'classification_name',
                                      'property_type_name', 'area']])
             y = data['price']
-            model = RandomForestRegressor(n_estimators=100, random_state=82)
+            model = RandomForestRegressor(n_estimators=100, random_state=402)
            # model = LinearRegression()
             #model = SVR()
            #model = KNeighborsRegressor(n_neighbors=3)
