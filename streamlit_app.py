ff86be5631c3ee0167760d9667bd95490753316d
M7MD <slw6341@gmail.com>
2025-04-20 17:00:09 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -337,8 +337,7 @@ if not df.empty:
             X = pd.get_dummies(data[['neighborhood_name', 'classification_name',
                                      'property_type_name', 'area']])
             y = data['price']
-            
-        model = RandomForestRegressor(n_estimators=100, random_state=42)
+            model = RandomForestRegressor(n_estimators=100, random_state=42)
         
            # model = LinearRegression()
             #model = SVR()
